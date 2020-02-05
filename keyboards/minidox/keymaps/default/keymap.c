28b3b3ec0f3c94bb80e681d5658beab145dc75f1
James Young <xxiinophobia@yahoo.com>
2020-02-05 03:16:40 -0800

    use layer_names enum in minidox default keymap

--- keyboards/minidox/keymaps/default/keymap.c
+++ keyboards/minidox/keymaps/default/keymap.c
@@ -1,14 +1,11 @@
 #include QMK_KEYBOARD_H
 
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 16
+enum layer_names {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _ADJUST
+};
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
