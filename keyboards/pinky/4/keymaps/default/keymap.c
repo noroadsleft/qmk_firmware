5d2851415a4d2d1f5e057488950ca906be844951
James Young <xxiinophobia@yahoo.com>
2020-02-05 03:16:45 -0800

    use layer_names enum in pinky/4 default keymap

--- keyboards/pinky/4/keymaps/default/keymap.c
+++ keyboards/pinky/4/keymaps/default/keymap.c
@@ -15,14 +15,12 @@
  */
 #include QMK_KEYBOARD_H
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-#define _QWERTY 0
-#define _LOWER 3
-#define _RAISE 4
-#define _ADJUST 16
+enum layer_names {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _ADJUST
+};
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
