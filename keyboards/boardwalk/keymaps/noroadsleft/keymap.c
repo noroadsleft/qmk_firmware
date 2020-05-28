df4bf47f297e2bf228876d434befb5a193a94a94
James Young <18669334+noroadsleft@users.noreply.github.com>
2020-05-28 03:33:36 -0700

    keymap cleanup

--- keyboards/boardwalk/keymaps/noroadsleft/keymap.c
+++ keyboards/boardwalk/keymaps/noroadsleft/keymap.c
@@ -19,19 +19,19 @@
 // Layer shorthand
 enum layers {
     // BASE LAYERS
-    _QWERTY = 0,
-    _DVORAK,
-    _COLEMAK,
-    _MAC,
+    _QWERTY,
+    // _DVORAK,
+    // _COLEMAK,
+    // _MAC,
 
     // FUNCTION LAYERS
     _FUNC_L,
     _FUNC_R,
 
     // OTHER LAYERS
-    _NUMPAD,
-    _MACROS,
-    _SYSTEM
+    // _NUMPAD,
+    // _MACROS,
+    // _SYSTEM
 };
 
 
@@ -85,27 +85,27 @@ enum macros {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT_2x2u_c(
-        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,      KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,      KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
-        FN_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME,      KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,       KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-        KC_LCTL, C_NUBS,  KC_LGUI, FN,      KC_LALT,          L_BSPC,       R_SPC,            KC_RALT, FN,      KC_RGUI, KC_BSLS, KC_RCTL  \
+        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,      KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,      KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+        FN_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME,      KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,       KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL, C_NUBS,  KC_LGUI, FN,      KC_LALT,          L_BSPC,       R_SPC,            KC_RALT, FN,      KC_RGUI, KC_BSLS, KC_RCTL
     ),
 
     [_FUNC_L] = LAYOUT_2x2u_c(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,       KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
-        _______, _______, _______, _______, _______, _______, KC_PSCR,      KC_SLCK, KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, \
-        _______, _______, _______, _______, _______, _______, _______,      _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, \
-        _______, _______, _______, _______, _______, _______, _______,      _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, \
-        _______, _______, _______, KC_TRNS, _______,          KC_TRNS,      KC_DEL,           _______, _______, _______, _______, _______  \
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,       KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+        _______, _______, _______, _______, _______, _______, KC_PSCR,      KC_SLCK, KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______,
+        _______, _______, _______, _______, _______, _______, _______,      _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,
+        _______, _______, _______, _______, _______, _______, _______,      _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
+        _______, _______, _______, KC_TRNS, _______,          KC_TRNS,      KC_DEL,           _______, _______, _______, _______, _______
     ),
 
     [_FUNC_R] = LAYOUT_2x2u_c(
-        RESET,   DEBUG,   VRSN,    _______, _______, _______, KC_MINS,      _______, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_0,    _______, \
-        _______, _______, _______, _______, _______, KC_VOLU, KC_LBRC,      _______, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_P,    _______, \
-        _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, KC_VOLD, KC_HOME,      _______, KC_PMNS, KC_P1,   KC_P2,   KC_P3,   KC_SCLN, KC_PENT, \
-        _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_MUTE, KC_END,       _______, KC_PPLS, KC_P0,   KC_P00,  KC_PDOT, KC_SLSH, _______, \
-        _______, _______, _______, _______, _______,          _______,      KC_TRNS,          _______, _______, _______, _______, _______  \
+        RESET,   DEBUG,   VRSN,    _______, _______, _______, KC_MINS,      _______, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_0,    _______,
+        _______, _______, _______, _______, _______, KC_VOLU, KC_LBRC,      _______, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_P,    _______,
+        _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, KC_VOLD, KC_HOME,      _______, KC_PMNS, KC_P1,   KC_P2,   KC_P3,   KC_SCLN, KC_PENT,
+        _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_MUTE, KC_END,       _______, KC_PPLS, KC_P0,   KC_P00,  KC_PDOT, KC_SLSH, _______,
+        _______, _______, _______, _______, _______,          _______,      KC_TRNS,          _______, _______, _______, _______, _______
     )
 
 };
@@ -175,16 +175,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record) {
             return false;
     }
     return true;
-}
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-void led_set_user(uint8_t usb_led) {
-
 }
