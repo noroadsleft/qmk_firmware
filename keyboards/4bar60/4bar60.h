3ac53e578260f2ec5ace1c033ff84a5a57ed1526
James Young <18669334+noroadsleft@users.noreply.github.com>
2020-11-06 22:27:42 -0800

    move encoder position to layout macro's top physical row

--- keyboards/4bar60/4bar60.h
+++ keyboards/4bar60/4bar60.h
@@ -19,8 +19,8 @@
 #include "quantum.h"
 
 #define LAYOUT( \
-    K00, K01, K02, K03, K04, K05,    K07, K08, K09,    K0B, K0C, K0D, K0E, K0F, K0H, \
-    K10, K11, K12, K13, K14, K15,    K17, K18, K19,    K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, \
+    K00, K01, K02, K03, K04, K05,    K07, K08, K09,    K0B, K0C, K0D, K0E, K0F, K0H,  K1H, \
+    K10, K11, K12, K13, K14, K15,    K17, K18, K19,    K1A, K1B, K1C, K1D, K1E, K1F, K1G, \
     K21, K22, K23, K24, K25, K26,    K27, K28, K29,    K2B, K2C, K2D, K2E, K2F, K2G, \
     K31,      K32, K33, K35,         K37, K38, K39,    K3B, K3C, K3E,      K3F, K3G, \
                                      K47, K48, K49                                   \
