49d8dbe5abf8ac84c66bb425f7093051d467d4f7
James Young <18669334+noroadsleft@users.noreply.github.com>
2020-11-06 22:13:56 -0800

    use KC_NO instead of ___ in layout macro

--- keyboards/4bar60/4bar60.h
+++ keyboards/4bar60/4bar60.h
@@ -18,8 +18,6 @@
 
 #include "quantum.h"
 
-#define ___ KC_NO
-
 #define LAYOUT( \
     K00, K01, K02, K03, K04, K05,    K07, K08, K09,    K0B, K0C, K0D, K0E, K0F, K0H, \
     K10, K11, K12, K13, K14, K15,    K17, K18, K19,    K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, \
@@ -28,9 +26,9 @@
                                      K47, K48, K49                                   \
 ) \
 { \
-    { K00, K01, K02, K03, K04, K05, ___, K07, K08, K09, ___, K0B, K0C, K0D, K0E, K0F, ___, K0H }, \
-    { K10, K11, K12, K13, K14, K15, ___, K17, K18, K19, K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H }, \
-    { ___, K21, K22, K23, K24, K25, K26, K27, K28, K29, ___, K2B, K2C, K2D, K2E, K2F, K2G, ___ }, \
-    { ___, K31, K32, K33, ___, K35, ___, K37, K38, K39, ___, K3B, K3C, ___, K3E, K3F, K3G, ___ }, \
-    { ___, ___, ___, ___, ___, ___, ___, K47, K48, K49, ___, ___, ___, ___, ___, ___, ___, ___ }  \
+    { K00,   K01,   K02,   K03,   K04,   K05,   KC_NO, K07, K08, K09, KC_NO, K0B,   K0C,   K0D,   K0E,   K0F,   KC_NO, K0H   }, \
+    { K10,   K11,   K12,   K13,   K14,   K15,   KC_NO, K17, K18, K19, K1A,   K1B,   K1C,   K1D,   K1E,   K1F,   K1G,   K1H   }, \
+    { KC_NO, K21,   K22,   K23,   K24,   K25,   K26,   K27, K28, K29, KC_NO, K2B,   K2C,   K2D,   K2E,   K2F,   K2G,   KC_NO }, \
+    { KC_NO, K31,   K32,   K33,   KC_NO, K35,   KC_NO, K37, K38, K39, KC_NO, K3B,   K3C,   KC_NO, K3E,   K3F,   K3G,   KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, K47, K48, K49, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }  \
 }
