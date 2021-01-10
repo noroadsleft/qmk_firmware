ba06e8ff1059c2eede26617ce55ca0da9e593329
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-01-10 11:40:56 -0800

    KBD67 rev1: enable 65_ansi and 65_iso community layouts

--- keyboards/kbdfans/kbd67/rev1/rules.mk
+++ keyboards/kbdfans/kbd67/rev1/rules.mk
@@ -31,4 +31,4 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 
-LAYOUTS = 65_ansi
+LAYOUTS = 65_ansi 65_iso
