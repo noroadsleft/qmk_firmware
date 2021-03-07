6869c87152c159e2365643a22455a538197c241b
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-03-06 19:22:53 -0800

    fix layout macro/keymap keycode ordering

--- keyboards/gmmk/keymaps/default/keymap.c
+++ keyboards/gmmk/keymaps/default/keymap.c
@@ -30,10 +30,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [0] = LAYOUT_gmmk_pro(
-    KC_LSFT,   KC_MUTE,  _______,  KC_LEFT,    KC_RCTL,     KC_RGHT,    KC_LCTL,   KC_F5,    KC_R,   KC_T,      KC_F,     KC_G,       KC_V,     KC_B,    KC_5,    KC_4,      KC_P,  KC_LBRC,  KC_SCLN,  KC_QUOT,   _______,   KC_SLSH,  KC_MINS,    KC_0,
-    KC_Q,      KC_TAB,   KC_A,      KC_ESC,       KC_Z,     KC_PGUP,     KC_GRV,    KC_1,    KC_U,   KC_Y,      KC_J,     KC_H,       KC_M,     KC_N,    KC_6,    KC_7,   KC_LWIN,  KC_RSFT,   KC_APP,  KC_LOPT,    KC_SPC,   KC_RALT,  _______, KC_PSCR,
-    KC_W,      KC_LCAP,  KC_S,       KC_RO,       KC_X,     KC_PGDN,      KC_F1,    KC_2,    KC_I,   KC_RBRC,   KC_K,    KC_F6,    KC_COMM,   KC_DEL,  KC_EQL,    KC_8,   _______,  KC_BSPC,  KC_PIPE,   KC_F11,    KC_ENT,    KC_F12,    KC_F9,  KC_F10,
-    KC_E,      KC_F3,    KC_D,       KC_F4,       KC_C,       KC_UP,      KC_F2,    KC_3,    KC_O,   KC_F7,     KC_L,  KC_DOWN,     KC_DOT,   KC_END,   KC_F8,    KC_9
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_MUTE,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_PIPE, KC_PGUP,
+    KC_LCAP, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,   KC_RSFT, KC_UP,   KC_END,
+    KC_LCTL, KC_LWIN, KC_LOPT,                            KC_SPC,                    KC_RALT, KC_APP,  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
 )
 
 };
--- keyboards/gmmk/pro/pro.h
+++ keyboards/gmmk/pro/pro.h
@@ -18,22 +18,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "gmmk.h"
 
-#define LAYOUT_gmmk_pro( \
-    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c, k0d, k0e, k0f, k0g, k0h, k0i, k0j, k0k, k0l, k0m, k0n,\
-    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k1e, k1f, k1g, k1h, k1i, k1j, k1k, k1l, k1m, k1n,\
-    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, k2e, k2f, k2g, k2h, k2i, k2j, k2k, k2l, k2m, k2n,\
-    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, k3e, k3f \
+#define LAYOUT_all( \
+    k13, k26, k36, k31, k33, k07, k2b, k39, k3e, k2m, k2n, k2j, k2l, k1n,     k01, \
+    k16, k17, k27, k37, k0f, k0e, k1e, k1f, k2f, k3f, k0n, k0m, k2e, k2h,     k2d, \
+    k11, k10, k20, k30, k08, k09, k19, k18, k28, k38, k0g, k0h, k29, k2i,     k15, \
+    k21, k12, k22, k32, k0a, k0b, k1b, k1a, k2a, k3a, k0i, k0j,      k2k,     k25, \
+    k00, k14, k24, k34, k0c, k0d, k1d, k1c, k2c, k3c, k0l, k23, k1h,     k35, k3d, \
+    k06, k1g, k1j,               k1k,               k1l, k1i, k04,  k03, k3b, k05  \
 ) \
 { \
-    { k00, k01, k02, k03, k04, k05, k06, k07}, \
-    { k10, k11, k12, k13, k14, k15, k16, k17}, \
-    { k20, k21, k22, k23, k24, k25, k26, k27}, \
-    { k30, k31, k32, k33, k34, k35, k36, k37}, \
-    { k08, k09, k0a, k0b, k0c, k0d, k0e, k0f}, \
-    { k18, k19, k1a, k1b, k1c, k1d, k1e, k1f}, \
-    { k28, k29, k2a, k2b, k2c, k2d, k2e, k2f}, \
-    { k38, k39, k3a, k3b, k3c, k3d, k3e, k3f}, \
-    { k0g, k0h, k0i, k0j, k0k, k0l, k0m, k0n}, \
-    { k1g, k1h, k1i, k1j, k1k, k1l, k1m, k1n}, \
-    { k2g, k2h, k2i, k2j, k2k, k2l, k2m, k2n}  \
+    { k00,   k01,   KC_NO, k03,   k04,   k05,   k06,   k07 }, \
+    { k10,   k11,   k12,   k13,   k14,   k15,   k16,   k17 }, \
+    { k20,   k21,   k22,   k23,   k24,   k25,   k26,   k27 }, \
+    { k30,   k31,   k32,   k33,   k34,   k35,   k36,   k37 }, \
+    { k08,   k09,   k0a,   k0b,   k0c,   k0d,   k0e,   k0f }, \
+    { k18,   k19,   k1a,   k1b,   k1c,   k1d,   k1e,   k1f }, \
+    { k28,   k29,   k2a,   k2b,   k2c,   k2d,   k2e,   k2f }, \
+    { k38,   k39,   k3a,   k3b,   k3c,   k3d,   k3e,   k3f }, \
+    { k0g,   k0h,   k0i,   k0j,   KC_NO, k0l,   k0m,   k0n }, \
+    { k1g,   k1h,   k1i,   k1j,   k1k,   k1l,   KC_NO, k1n }, \
+    { KC_NO, k2h,   k2i,   k2j,   k2k,   k2l,   k2m,   k2n }  \
 }
