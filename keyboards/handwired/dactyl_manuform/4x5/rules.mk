c92c5fc7a9f08a80586c10b785af5458d4018c13
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-06-26 10:49:00 -0700

    format version rules.mk files, phase 1b
    Formats the files edited in Phase 1 to match the main rules.mk file.

--- keyboards/handwired/dactyl_manuform/4x5/rules.mk
+++ keyboards/handwired/dactyl_manuform/4x5/rules.mk
@@ -1,3 +1,9 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
 # Build Options
 #   change yes to no to disable
 #
@@ -14,3 +20,5 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
+
+SPLIT_KEYBOARD = yes
