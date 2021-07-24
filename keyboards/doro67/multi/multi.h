0a79b1ea5d45ee86dd03fcb30a5e267e3a43e675
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-07-24 10:55:58 -0700

    doro67/multi: layout macro/info.json fixes
    - rename LAYOUT_iso to LAYOUT_65_iso_blocker
    - rename LAYOUT_multi to LAYOUT_all

--- keyboards/doro67/multi/multi.h
+++ keyboards/doro67/multi/multi.h
@@ -31,7 +31,7 @@
     { k40,   k41,   k42,   KC_NO, k44,   KC_NO, KC_NO, KC_NO, KC_NO, k49,   k4A,   KC_NO, k4C,   k4D,   k4E   }, \
 }
 
-#define LAYOUT_iso( \
+#define LAYOUT_65_iso_blocker( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D,      k0E, \
     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C,           k1E, \
     k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k1D, k2D,      k2E, \
@@ -45,7 +45,7 @@
     { k40,   k41,   k42,   KC_NO, k44,   KC_NO, KC_NO, KC_NO, KC_NO, k49,   k4A,   KC_NO, k4C,   k4D,   k4E   }, \
 }
 
-#define LAYOUT_multi( \
+#define LAYOUT_all( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, k48, k0E, \
     k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D,      k1E, \
     k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B,      k2D,      k2E, \
