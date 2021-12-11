58e28f9ef43c263ec859cbeb96752003f18a6369
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-12-10 19:24:55 -0800

    update 60_ansi_arrow keymap

--- layouts/default/60_ansi_arrow/default_60_ansi_arrow/keymap.c
+++ layouts/default/60_ansi_arrow/default_60_ansi_arrow/keymap.c
@@ -2,6 +2,7 @@
  * Default 60 ANSI Arrow Keymap.
  *
  * Copyright (C) 2020 Sendy YK <https://mr.sendyyk.com>
+ *               2021 QMK
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,10 +21,14 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _BASE
+    _BASE,
+    _FN
 };
 
+#define FN_APP LT(1, KC_APP)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
     /*
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
      * │Esc│1  │2  │3  │4  │5  │6  │7  │8  │9  │0  │-  │+  │Bspc   │
@@ -34,14 +39,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴┬───┬───┤
      * │Shift   │Z  │X  │C  │V  │B  │N  │M  │,  │.  │Shift │↑  │/  │
      * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬───┼───┼───┤
-     * │Ctrl│GUI │Alt │Space                   │Alt│GUI│←  │↓  │→  │
+     * │Ctrl│GUI │Alt │Space                   │Alt│ Fn│←  │↓  │→  │
      * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┘
      */
     [_BASE] = LAYOUT_60_ansi_arrow(
-        KC_GESC, KC_1,    KC_2, KC_3,    KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
-        KC_TAB,           KC_Q, KC_W,    KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_CAPS,          KC_A, KC_S,    KC_D, KC_F, KC_G,   KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSFT,          KC_Z, KC_X,    KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,           KC_RSFT, KC_UP,   KC_SLSH,
-        KC_LCTL, KC_LGUI,       KC_LALT,             KC_SPC,                      KC_RALT, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, KC_UP,   KC_SLSH,
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, FN_APP,  KC_LEFT, KC_DOWN, KC_RGHT
     ),
+
+    /*
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+     * │ ` │F1 │F2 │F3 │F4 │F5 │F6 │F7 │F8 │F9 │F10│F11│F12│ Delete│
+     * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+     * │     │   │↑  │   │   │   │Ins│   │   │   │   │Prt│SLk│Pause│
+     * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
+     * │      │←  │↓  │→  │   │   │Del│   │   │   │   │   │        │
+     * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴┬───┬───┤
+     * │        │   │   │   │   │   │   │   │   │   │      │PgU│   │
+     * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬───┼───┼───┤
+     * │    │    │    │                        │   │   │Hom│PgD│End│
+     * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┘
+     */
+    [_FN] = LAYOUT_60_ansi_arrow(
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        _______, _______, KC_UP,   _______, _______, _______, KC_INS,  _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_DEL,  _______, _______, _______, _______, _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, _______,
+        _______, _______, _______,                            _______,                   _______, _______, KC_HOME, KC_PGDN, KC_END
+    ),
+
 };
