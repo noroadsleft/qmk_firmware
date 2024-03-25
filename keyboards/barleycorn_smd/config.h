2593eade163fcd2d78ae14b66cc9f6bc7a9460db
James Young <18669334+noroadsleft@users.noreply.github.com>
2024-03-25 12:45:37 -0700

    Data-driven conversion for `barleycorn_smd`

--- keyboards/barleycorn_smd/config.h
+++ keyboards/barleycorn_smd/config.h
@@ -17,11 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 18
-
 /*
  * Keyboard Matrix Assignments
  *
@@ -34,17 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* A Custom matrix.c is used to poll the port expander C6 shows that the pins are hardwired there */
-#define MATRIX_ROW_PINS { F5, F4, F1, F0, F6 }
-#define MATRIX_COL_PINS { D4, D6, D7, B4, B5, B6, C6, C7, D5, D5, D5, D5, D5, D5, D5, D5, D5, D5 }
 #define PORT_EXPANDER_ADDRESS 0x20
-
-/* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION COL2ROW
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
 
 /*
  * Feature disable options
