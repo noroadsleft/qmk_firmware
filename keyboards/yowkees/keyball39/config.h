405a3ade6e01f440db99a4759be594c9a71802ae
James Young <18669334+noroadsleft@users.noreply.github.com>
2024-05-25 04:56:32 -0700

    Define PMW33XX_CS_PIN

--- keyboards/yowkees/keyball39/config.h
+++ keyboards/yowkees/keyball39/config.h
@@ -77,3 +77,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // To squeeze firmware size
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
+
+
+
+// PMW3360 configuration
+#define PMW33XX_CS_PIN B6
