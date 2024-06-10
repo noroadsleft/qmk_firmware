b9db49de11200e6389660dc5df4f2d9f907b1f2a
James Young <18669334+noroadsleft@users.noreply.github.com>
2024-06-10 13:16:48 -0700

    Switch to core PMW3360 driver

--- keyboards/yowkees/keyball39/rules.mk
+++ keyboards/yowkees/keyball39/rules.mk
@@ -1,6 +1,5 @@
 # Optical sensor driver for trackball.
-POINTING_DEVICE_DRIVER = custom
-SRC += drivers/pmw3360/pmw3360.c
+POINTING_DEVICE_DRIVER = pmw3360
 SPI_DRIVER_REQUIRED = yes
 
 SRC += lib/oledkit/oledkit.c    # OLED utility for Keyball series.
