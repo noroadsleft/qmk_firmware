7c906af35492a0428b6574861e5ee43e1395fef9
James Young <18669334+noroadsleft@users.noreply.github.com>
2024-07-04 03:21:08 -0700

    Rename `RGB_DI_PIN` to `WS2812_DI_PIN`

--- keyboards/geigeigeist/klor/config.h
+++ keyboards/geigeigeist/klor/config.h
@@ -68,7 +68,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // │ r g b   l e d s                                 │
 // └─────────────────────────────────────────────────┘ 
 
-#define RGB_DI_PIN D3
+#define WS2812_DI_PIN D3
 #define RGBLED_NUM 42
 #define RGBLED_SPLIT { 21, 21 }
 #define RGB_MATRIX_LED_COUNT RGBLED_NUM
